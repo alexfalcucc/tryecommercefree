4ab4245d56d84dd73c1396cc52957fca862f1659
alexfalcucci <alex.falcucci@hotmail.com>
2014-06-18 21:35:19 -0300

    fir commit on deploy in the heroku too

--- ecommerce_free/urls.py
+++ ecommerce_free/urls.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -9,3 +10,5 @@ urlpatterns = patterns('',
     url(r'^$', 'ecommerce_free.core.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
